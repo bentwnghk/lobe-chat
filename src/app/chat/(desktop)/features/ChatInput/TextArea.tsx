a1e91551aa5c5763edd7a0aea410618983441a11
Ben <mr.ng@mystudies.net>
2024-01-09 17:23:39 +0800

    🐛 fix: fix rename

--- src/app/chat/(desktop)/features/ChatInput/TextArea.tsx
+++ src/app/chat/(desktop)/features/ChatInput/TextArea.tsx
@@ -1,4 +1,4 @@
-import { TextArea } from '@lobehub/ui';
+import { TextArea } from '@bentwnghk/ui';
 import { createStyles } from 'antd-style';
 import { TextAreaRef } from 'antd/es/input/TextArea';
 import { memo, useEffect, useRef } from 'react';
