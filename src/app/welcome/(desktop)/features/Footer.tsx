c4f2f305ed2af06b62e57d9b9291770159169579
Ben <mr.ng@mystudies.net>
2024-02-22 08:06:04 +0800

    🐛 fix: fix lint check error

--- src/app/welcome/(desktop)/features/Footer.tsx
+++ src/app/welcome/(desktop)/features/Footer.tsx
@@ -4,14 +4,14 @@ import { ActionIcon, DiscordIcon } from '@bentwnghk/ui';
 import { useTheme } from 'antd-style';
 import { Book, Github } from 'lucide-react';
 import { memo } from 'react';
-import { useTranslation } from 'react-i18next';
+// import { useTranslation } from 'react-i18next';
 import { Flexbox } from 'react-layout-kit';
 
 import { DISCORD, DOCUMENTS, GITHUB } from '@/const/url';
 
 const Footer = memo(() => {
   const theme = useTheme();
-  const { t } = useTranslation('common');
+//  const { t } = useTranslation('common');
 
   return (
     <Flexbox align={'center'} horizontal justify={'space-between'} style={{ padding: 16 }}>
