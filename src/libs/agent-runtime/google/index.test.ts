ecdbc4f63bc49ed76413710620b6621968fce990
Ben <mr.ng@mystudies.net>
2024-03-29 16:41:43 +0800

    Revert "🐛 fix: fix google gemini pro 1.5 #1801"
    This reverts commit 5517eefc025bcb59c112d2746e1fe1f608fad705.

--- src/libs/agent-runtime/google/index.test.ts
+++ src/libs/agent-runtime/google/index.test.ts
@@ -1,204 +1,364 @@
-import { Content, GoogleGenerativeAI, Part } from '@google/generative-ai';
-import { GoogleGenerativeAIStream, StreamingTextResponse } from 'ai';
+// @vitest-environment edge-runtime
+import { GenerateContentRequest, GenerateContentStreamResult, Part } from '@google/generative-ai';
+import OpenAI from 'openai';
+import { Mock, afterEach, beforeEach, describe, expect, it, vi } from 'vitest';
 
-import { LobeRuntimeAI } from '../BaseAI';
-import { AgentRuntimeErrorType, ILobeAgentRuntimeErrorType } from '../error';
-import {
-  ChatCompetitionOptions,
-  ChatStreamPayload,
-  OpenAIChatMessage,
-  UserMessageContentPart,
-} from '../types';
-import { ModelProvider } from '../types/type';
-import { AgentRuntimeError } from '../utils/createError';
-import { debugStream } from '../utils/debugStream';
-import { parseDataUri } from '../utils/uriParser';
+import { ChatStreamCallbacks, OpenAIChatMessage } from '@/libs/agent-runtime';
 
-enum HarmCategory {
-  HARM_CATEGORY_DANGEROUS_CONTENT = 'HARM_CATEGORY_DANGEROUS_CONTENT',
-  HARM_CATEGORY_HARASSMENT = 'HARM_CATEGORY_HARASSMENT',
-  HARM_CATEGORY_HATE_SPEECH = 'HARM_CATEGORY_HATE_SPEECH',
-  HARM_CATEGORY_SEXUALLY_EXPLICIT = 'HARM_CATEGORY_SEXUALLY_EXPLICIT',
-}
+import * as debugStreamModule from '../utils/debugStream';
+import { LobeGoogleAI } from './index';
 
-enum HarmBlockThreshold {
-  BLOCK_NONE = 'BLOCK_NONE',
-}
+const provider = 'google';
+const defaultBaseURL = 'https://api.moonshot.cn/v1';
+const bizErrorType = 'GoogleBizError';
+const invalidErrorType = 'InvalidGoogleAPIKey';
 
-export class LobeGoogleAI implements LobeRuntimeAI {
-  private client: GoogleGenerativeAI;
+// Mock the console.error to avoid polluting test output
+vi.spyOn(console, 'error').mockImplementation(() => {});
 
-  constructor({ apiKey }: { apiKey?: string }) {
-    if (!apiKey) throw AgentRuntimeError.createError(AgentRuntimeErrorType.InvalidGoogleAPIKey);
+let instance: LobeGoogleAI;
 
-    this.client = new GoogleGenerativeAI(apiKey);
-  }
+beforeEach(() => {
+  instance = new LobeGoogleAI({ apiKey: 'test' });
 
-  async chat(payload: ChatStreamPayload, options?: ChatCompetitionOptions) {
-    try {
-      const { contents, model } = this.buildGoogleMessages(payload.messages, payload.model);
-      const geminiStream = await this.client
-        .getGenerativeModel(
-          {
-            generationConfig: {
-              maxOutputTokens: payload.max_tokens,
-              temperature: payload.temperature,
-              topP: payload.top_p,
-            },
-            model,
-            // avoid wide sensitive words
-            // refs: https://github.com/lobehub/lobe-chat/pull/1418
-            safetySettings: [
+  // 使用 vi.spyOn 来模拟 chat.completions.create 方法
+  vi.spyOn(instance['client'], 'getGenerativeModel').mockReturnValue({
+    generateContentStream: vi.fn().mockResolvedValue(new ReadableStream()),
+  } as any);
+});
+
+afterEach(() => {
+  vi.clearAllMocks();
+});
+
+describe('LobeGoogleAI', () => {
+  describe('init', () => {
+    it('should correctly initialize with an API key', async () => {
+      const instance = new LobeGoogleAI({ apiKey: 'test_api_key' });
+      expect(instance).toBeInstanceOf(LobeGoogleAI);
+
+      // expect(instance.baseURL).toEqual(defaultBaseURL);
+    });
+  });
+
+  describe('chat', () => {
+    it('should return a StreamingTextResponse on successful API call', async () => {
+      const result = await instance.chat({
+        messages: [{ content: 'Hello', role: 'user' }],
+        model: 'text-davinci-003',
+        temperature: 0,
+      });
+
+      // Assert
+      expect(result).toBeInstanceOf(Response);
+    });
+    it('should handle text messages correctly', async () => {
+      // 模拟 Google AI SDK 的 generateContentStream 方法返回一个成功的响应流
+      const mockStream = new ReadableStream({
+        start(controller) {
+          controller.enqueue('Hello, world!');
+          controller.close();
+        },
+      });
+      vi.spyOn(instance['client'], 'getGenerativeModel').mockReturnValue({
+        generateContentStream: vi.fn().mockResolvedValueOnce(mockStream),
+      } as any);
+
+      const result = await instance.chat({
+        messages: [{ content: 'Hello', role: 'user' }],
+        model: 'text-davinci-003',
+        temperature: 0,
+      });
+
+      expect(result).toBeInstanceOf(Response);
+      // 额外的断言可以加入，比如验证返回的流内容等
+    });
+
+    it('should call debugStream in DEBUG mode', async () => {
+      // 设置环境变量以启用DEBUG模式
+      process.env.DEBUG_GOOGLE_CHAT_COMPLETION = '1';
+
+      // 模拟 Google AI SDK 的 generateContentStream 方法返回一个成功的响应流
+      const mockStream = new ReadableStream({
+        start(controller) {
+          controller.enqueue('Debug mode test');
+          controller.close();
+        },
+      });
+      vi.spyOn(instance['client'], 'getGenerativeModel').mockReturnValue({
+        generateContentStream: vi.fn().mockResolvedValueOnce(mockStream),
+      } as any);
+      const debugStreamSpy = vi
+        .spyOn(debugStreamModule, 'debugStream')
+        .mockImplementation(() => Promise.resolve());
+
+      await instance.chat({
+        messages: [{ content: 'Hello', role: 'user' }],
+        model: 'text-davinci-003',
+        temperature: 0,
+      });
+
+      expect(debugStreamSpy).toHaveBeenCalled();
+
+      // 清理环境变量
+      delete process.env.DEBUG_GOOGLE_CHAT_COMPLETION;
+    });
+
+    describe('Error', () => {
+      it('should throw InvalidGoogleAPIKey error on API_KEY_INVALID error', async () => {
+        // 模拟 Google AI SDK 抛出异常
+        const message = `[GoogleGenerativeAI Error]: Error fetching from https://generativelanguage.googleapis.com/v1/models/gemini-pro:streamGenerateContent?alt=sse: [400 Bad Request] API key not valid. Please pass a valid API key. [{"@type":"type.googleapis.com/google.rpc.ErrorInfo","reason":"API_KEY_INVALID","domain":"googleapis.com","metadata":{"service":"generativelanguage.googleapis.com"}}]`;
+
+        const apiError = new Error(message);
+
+        vi.spyOn(instance['client'], 'getGenerativeModel').mockReturnValue({
+          generateContentStream: vi.fn().mockRejectedValue(apiError),
+        } as any);
+
+        try {
+          await instance.chat({
+            messages: [{ content: 'Hello', role: 'user' }],
+            model: 'text-davinci-003',
+            temperature: 0,
+          });
+        } catch (e) {
+          expect(e).toEqual({ errorType: invalidErrorType, error: { message }, provider });
+        }
+      });
+
+      it('should throw LocationNotSupportError error on location not support error', async () => {
+        // 模拟 Google AI SDK 抛出异常
+        const message = `[GoogleGenerativeAI Error]: Error fetching from https://generativelanguage.googleapis.com/v1/models/gemini-pro:streamGenerateContent?alt=sse: [400 Bad Request] User location is not supported for the API use.`;
+
+        const apiError = new Error(message);
+
+        vi.spyOn(instance['client'], 'getGenerativeModel').mockReturnValue({
+          generateContentStream: vi.fn().mockRejectedValue(apiError),
+        } as any);
+
+        try {
+          await instance.chat({
+            messages: [{ content: 'Hello', role: 'user' }],
+            model: 'text-davinci-003',
+            temperature: 0,
+          });
+        } catch (e) {
+          expect(e).toEqual({ errorType: 'LocationNotSupportError', error: { message }, provider });
+        }
+      });
+
+      it('should throw BizError error', async () => {
+        // 模拟 Google AI SDK 抛出异常
+        const message = `[GoogleGenerativeAI Error]: Error fetching from https://generativelanguage.googleapis.com/v1/models/gemini-pro:streamGenerateContent?alt=sse: [400 Bad Request] API key not valid. Please pass a valid API key. [{"@type":"type.googleapis.com/google.rpc.ErrorInfo","reason":"Error","domain":"googleapis.com","metadata":{"service":"generativelanguage.googleapis.com"}}]`;
+
+        const apiError = new Error(message);
+
+        vi.spyOn(instance['client'], 'getGenerativeModel').mockReturnValue({
+          generateContentStream: vi.fn().mockRejectedValue(apiError),
+        } as any);
+
+        try {
+          await instance.chat({
+            messages: [{ content: 'Hello', role: 'user' }],
+            model: 'text-davinci-003',
+            temperature: 0,
+          });
+        } catch (e) {
+          expect(e).toEqual({
+            errorType: bizErrorType,
+            error: [
               {
-                category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
-                threshold: HarmBlockThreshold.BLOCK_NONE,
-              },
-              {
-                category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
-                threshold: HarmBlockThreshold.BLOCK_NONE,
-              },
-              {
-                category: HarmCategory.HARM_CATEGORY_HARASSMENT,
-                threshold: HarmBlockThreshold.BLOCK_NONE,
-              },
-              {
-                category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
-                threshold: HarmBlockThreshold.BLOCK_NONE,
+                '@type': 'type.googleapis.com/google.rpc.ErrorInfo',
+                'domain': 'googleapis.com',
+                'metadata': {
+                  service: 'generativelanguage.googleapis.com',
+                },
+                'reason': 'Error',
               },
             ],
-          },
-          {
-            apiVersion: 'v1beta',
-          },
-        )
-        .generateContentStream({ contents });
-
-      // Convert the response into a friendly text-stream
-      const stream = GoogleGenerativeAIStream(geminiStream, options?.callback);
-
-      const [debug, output] = stream.tee();
-
-      if (process.env.DEBUG_GOOGLE_CHAT_COMPLETION === '1') {
-        debugStream(debug).catch(console.error);
-      }
-
-      // Respond with the stream
-      return new StreamingTextResponse(output, { headers: options?.headers });
-    } catch (e) {
-      const err = e as Error;
-
-      const { errorType, error } = this.parseErrorMessage(err.message);
-
-      throw AgentRuntimeError.chat({ error, errorType, provider: ModelProvider.Google });
-    }
-  }
-
-  private convertContentToGooglePart = (content: UserMessageContentPart): Part => {
-    switch (content.type) {
-      case 'text': {
-        return { text: content.text };
-      }
-      case 'image_url': {
-        const { mimeType, base64 } = parseDataUri(content.image_url.url);
-
-        if (!base64) {
-          throw new TypeError("Image URL doesn't contain base64 data");
+            provider,
+          });
         }
+      });
 
-        return {
-          inlineData: {
-            data: base64,
-            mimeType: mimeType || 'image/png',
+      it('should throw DefaultError error', async () => {
+        // 模拟 Google AI SDK 抛出异常
+        const message = `[GoogleGenerativeAI Error]: Error fetching from https://generativelanguage.googleapis.com/v1/models/gemini-pro:streamGenerateContent?alt=sse: [400 Bad Request] API key not valid. Please pass a valid API key. [{"@type":"type.googleapis.com/google.rpc.ErrorInfo","reason":"Error","domain":"googleapis.com","metadata":{"service":"generativelanguage.googleapis.com}}]`;
+
+        const apiError = new Error(message);
+
+        vi.spyOn(instance['client'], 'getGenerativeModel').mockReturnValue({
+          generateContentStream: vi.fn().mockRejectedValue(apiError),
+        } as any);
+
+        try {
+          await instance.chat({
+            messages: [{ content: 'Hello', role: 'user' }],
+            model: 'text-davinci-003',
+            temperature: 0,
+          });
+        } catch (e) {
+          expect(e).toEqual({
+            errorType: bizErrorType,
+            error: {
+              message: `[GoogleGenerativeAI Error]: Error fetching from https://generativelanguage.googleapis.com/v1/models/gemini-pro:streamGenerateContent?alt=sse: [400 Bad Request] API key not valid. Please pass a valid API key. [{"@type":"type.googleapis.com/google.rpc.ErrorInfo","reason":"Error","domain":"googleapis.com","metadata":{"service":"generativelanguage.googleapis.com}}]`,
+            },
+            provider,
+          });
+        }
+      });
+
+      it('should return GoogleBizError with an openai error response when APIError is thrown', async () => {
+        // Arrange
+        const apiError = new Error('Error message');
+
+        // 使用 vi.spyOn 来模拟 chat.completions.create 方法
+        vi.spyOn(instance['client'], 'getGenerativeModel').mockReturnValue({
+          generateContentStream: vi.fn().mockRejectedValue(apiError),
+        } as any);
+
+        // Act
+        try {
+          await instance.chat({
+            messages: [{ content: 'Hello', role: 'user' }],
+            model: 'text-davinci-003',
+            temperature: 0,
+          });
+        } catch (e) {
+          expect(e).toEqual({
+            error: { message: 'Error message' },
+            errorType: bizErrorType,
+            provider,
+          });
+        }
+      });
+
+      it('should throw AgentRuntimeError with NoOpenAIAPIKey if no apiKey is provided', async () => {
+        try {
+          new LobeGoogleAI({});
+        } catch (e) {
+          expect(e).toEqual({ errorType: invalidErrorType });
+        }
+      });
+
+      it('should return OpenAIBizError with the cause when OpenAI.APIError is thrown with cause', async () => {
+        // Arrange
+        const errorInfo = {
+          stack: 'abc',
+          cause: {
+            message: 'api is undefined',
           },
         };
-      }
-    }
-  };
+        const apiError = new OpenAI.APIError(400, errorInfo, 'module error', {});
 
-  private convertOAIMessagesToGoogleMessage = (message: OpenAIChatMessage): Content => {
-    const content = message.content as string | UserMessageContentPart[];
+        vi.spyOn(instance['client'], 'getGenerativeModel').mockReturnValue({
+          generateContentStream: vi.fn().mockRejectedValue(apiError),
+        } as any);
 
-    return {
-      parts:
-        typeof content === 'string'
-          ? [{ text: content }]
-          : content.map((c) => this.convertContentToGooglePart(c)),
-      role: message.role === 'user' ? 'user' : 'model',
-    };
-  };
-
-  // convert messages from the Vercel AI SDK Format to the format
-  // that is expected by the Google GenAI SDK
-  private buildGoogleMessages = (
-    messages: OpenAIChatMessage[],
-    model: string,
-  ): { contents: Content[]; model: string } => {
-    const contents = messages
-      .filter((message) => message.role === 'user' || message.role === 'assistant')
-      .map((msg) => this.convertOAIMessagesToGoogleMessage(msg));
-
-    // if message are all text message, use vision will return error
-    // use add an image to use models/gemini-pro-vision, or switch your model to a text model
-    const noImage = messages.every((m) => typeof m.content === 'string');
-
-    return { contents, model: noImage ? 'gemini-pro' : model };
-  };
-
-  private parseErrorMessage(message: string): {
-    error: any;
-    errorType: ILobeAgentRuntimeErrorType;
-  } {
-    const defaultError = {
-      error: { message },
-      errorType: AgentRuntimeErrorType.GoogleBizError,
-    };
-
-    if (message.includes('location is not supported'))
-      return { error: { message }, errorType: AgentRuntimeErrorType.LocationNotSupportError };
-
-    try {
-      const startIndex = message.lastIndexOf('[');
-      if (startIndex === -1) {
-        return defaultError;
-      }
-
-      // 从开始位置截取字符串到最后
-      const jsonString = message.slice(startIndex);
-
-      // 尝试解析 JSON 字符串
-      const json: GoogleChatErrors = JSON.parse(jsonString);
-
-      const bizError = json[0];
-
-      switch (bizError.reason) {
-        case 'API_KEY_INVALID': {
-          return { ...defaultError, errorType: AgentRuntimeErrorType.InvalidGoogleAPIKey };
+        // Act
+        try {
+          await instance.chat({
+            messages: [{ content: 'Hello', role: 'user' }],
+            model: 'text-davinci-003',
+            temperature: 0,
+          });
+        } catch (e) {
+          expect(e).toEqual({
+            error: {
+              message: `400 {"stack":"abc","cause":{"message":"api is undefined"}}`,
+            },
+            errorType: bizErrorType,
+            provider,
+          });
         }
+      });
 
-        default: {
-          return { error: json, errorType: AgentRuntimeErrorType.GoogleBizError };
+      it('should return AgentRuntimeError for non-OpenAI errors', async () => {
+        // Arrange
+        const genericError = new Error('Generic Error');
+
+        vi.spyOn(instance['client'], 'getGenerativeModel').mockReturnValue({
+          generateContentStream: vi.fn().mockRejectedValue(genericError),
+        } as any);
+
+        // Act
+        try {
+          await instance.chat({
+            messages: [{ content: 'Hello', role: 'user' }],
+            model: 'text-davinci-003',
+            temperature: 0,
+          });
+        } catch (e) {
+          expect(e).toEqual({
+            errorType: 'GoogleBizError',
+            provider,
+            error: {
+              message: 'Generic Error',
+            },
+          });
         }
-      }
-    } catch {
-      // 如果解析失败，则返回原始错误消息
-      return defaultError;
-    }
-  }
+      });
+    });
+  });
 
-  private shouldUseBetaApi(model: string) {
-    return ['gemini-1.5-pro-latest', 'gemini-ultra-latest'].includes(model);
-  }
-}
+  describe('private method', () => {
+    describe('convertContentToGooglePart', () => {
+      it('should throw TypeError when image URL does not contain base64 data', () => {
+        // 提供一个不包含base64数据的图像URL
+        const invalidImageUrl = 'http://example.com/image.png';
 
-export default LobeGoogleAI;
+        expect(() =>
+          instance['convertContentToGooglePart']({
+            type: 'image_url',
+            image_url: { url: invalidImageUrl },
+          }),
+        ).toThrow(TypeError);
+      });
+    });
 
-type GoogleChatErrors = GoogleChatError[];
+    describe('buildGoogleMessages', () => {
+      it('should use default text model when no images are included in messages', () => {
+        const messages: OpenAIChatMessage[] = [
+          { content: 'Hello', role: 'user' },
+          { content: 'Hi', role: 'assistant' },
+        ];
+        const model = 'text-davinci-003';
 
-interface GoogleChatError {
-  '@type': string;
-  'domain': string;
-  'metadata': {
-    service: string;
-  };
-  'reason': string;
-}
+        // 调用 buildGoogleMessages 方法
+        const { contents, model: usedModel } = instance['buildGoogleMessages'](messages, model);
+
+        expect(usedModel).toEqual('gemini-pro'); // 假设 'gemini-pro' 是默认文本模型
+        expect(contents).toHaveLength(2);
+        expect(contents).toEqual([
+          { parts: [{ text: 'Hello' }], role: 'user' },
+          { parts: [{ text: 'Hi' }], role: 'model' },
+        ]);
+      });
+
+      it('should use specified model when images are included in messages', () => {
+        const messages: OpenAIChatMessage[] = [
+          {
+            content: [
+              { type: 'text', text: 'Hello' },
+              { type: 'image_url', image_url: { url: 'data:image/png;base64,...' } },
+            ],
+            role: 'user',
+          },
+        ];
+        const model = 'gemini-pro-vision';
+
+        // 调用 buildGoogleMessages 方法
+        const { contents, model: usedModel } = instance['buildGoogleMessages'](messages, model);
+
+        expect(usedModel).toEqual(model);
+        expect(contents).toHaveLength(1);
+        expect(contents).toEqual([
+          {
+            parts: [{ text: 'Hello' }, { inlineData: { data: '...', mimeType: 'image/png' } }],
+            role: 'user',
+          },
+        ]);
+      });
+    });
+  });
+});
