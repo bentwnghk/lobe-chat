e5ebec4a47d7edbf486d6ed8ac74fdd00ae1041d
Ben <mr.ng@mystudies.net>
2024-05-03 03:36:20 +0800

    Rename info

--- src/app/(main)/settings/about/index.tsx
+++ src/app/(main)/settings/about/index.tsx
@@ -1,11 +1,9 @@
 'use client';
 
 import { Logo, Tag } from '@bentwnghk/ui';
-import Link from 'next/link';
 import { memo } from 'react';
 import { Flexbox } from 'react-layout-kit';
 
-import { OFFICIAL_SITE, RELEASES_URL } from '@/const/url';
 import { CURRENT_VERSION } from '@/const/version';
 import Follow from '@/features/Follow';
 import { useServerConfigStore } from '@/store/serverConfig';
@@ -21,21 +19,17 @@ const Page = memo(({ mobile }: { mobile?: boolean }) => {
 
   return (
     <Flexbox align={'center'} gap={12} paddingBlock={36} width={'100%'}>
-      <Link href={OFFICIAL_SITE} target={'_blank'}>
-        <Logo size={mobile ? 100 : 120} />
-      </Link>
+      <Logo size={mobile ? 100 : 120} />
       <h1 style={{ fontSize: mobile ? 32 : 36, fontWeight: 900, lineHeight: 1, marginBottom: 0 }}>
-        LobeChat
+      Mr.🆖 Chat
       </h1>
-      <Link href={RELEASES_URL} target={'_blank'}>
-        <Tag>v{CURRENT_VERSION}</Tag>
-      </Link>
+      <Tag>v{CURRENT_VERSION}</Tag>
       <Flexbox gap={24} style={{ marginBlock: 48 }} width={'100%'}>
         <AboutList />
         {enabledTelemetryChat && <Analytics />}
       </Flexbox>
       <Follow />
-      <div>Empowering your AI dreams by LobeHub</div>
+      <div>Empowering your self-directed learning with AI</div>
       <div style={{ fontWeight: 400, opacity: 0.33 }}>{COPYRIGHT}</div>
     </Flexbox>
   );
