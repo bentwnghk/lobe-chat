67fbee66d580cde55056913809ff245ad3c08283
Ben <mr.ng@mystudies.net>
2024-06-14 22:20:46 +0800

    Update Footer.tsx

--- src/app/(main)/settings/features/Footer.tsx
+++ src/app/(main)/settings/features/Footer.tsx
@@ -85,7 +85,7 @@ const Footer = memo<PropsWithChildren>(() => {
         onCancel={() => setOpenFeedback(false)}
         onOk={() => {
           if (isOnServerSide) return;
-          window.open(GITHUB_ISSUES, '__parent');
+          window.open(GITHUB_ISSUES, '__self');
         }}
         open={openFeedback}
         title={t('footer.feedback.title')}
