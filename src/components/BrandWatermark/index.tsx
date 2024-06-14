bab82f6960976a5e79b8975ad31fbfe1023cff55
Ben <mr.ng@mystudies.net>
2024-06-14 20:46:55 +0800

    Update index.tsx

--- src/components/BrandWatermark/index.tsx
+++ src/components/BrandWatermark/index.tsx
@@ -29,7 +29,7 @@ const BrandWatermark = memo<Omit<FlexboxProps, 'children'>>(({ style, ...rest })
       {...rest}
     >
       <span>Powered by</span>
-      <Link className={styles.logoLink} href={'https://mr5drive.com'} target={'_blank'}>
+      <Link className={styles.logoLink} href={'https://api.mister5.net'} target={'_blank'}>
         <Logo size={20} type={'text'} />
       </Link>
     </Flexbox>
