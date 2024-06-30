83086145b84543da4c5bb7478268e25425251049
Ben Ng <mr.ng@mystudies.net>
2024-06-30 23:54:31 +0800

    Update index.ts

--- scripts/migrateServerDB/index.ts
+++ scripts/migrateServerDB/index.ts
@@ -1,15 +1,21 @@
 import * as dotenv from 'dotenv';
-import * as migrator from 'drizzle-orm/neon-serverless/migrator';
+// import * as migrator from 'drizzle-orm/neon-serverless/migrator';
+import { migrate } from 'drizzle-orm/neon-http/migrator';
 import { join } from 'node:path';
 
-import { serverDB } from '../../src/database/server/core/db';
+// import { serverDB } from '../../src/database/server/core/db';
+import { drizzle } from 'drizzle-orm/neon-http';
+import { neon } from '@neondatabase/serverless';
 
 // Read the `.env` file if it exists, or a file specified by the
 // dotenv_config_path parameter that's passed to Node.js
 dotenv.config();
+const sql = neon(process.env.DATABASE_URL!);
+const serverDB = drizzle(sql);
 
 const runMigrations = async () => {
-  await migrator.migrate(serverDB, {
+  // await migrator.migrate(serverDB, {
+  await migrate (serverDB, {
     migrationsFolder: join(__dirname, '../../src/database/server/migrations'),
   });
   console.log('✅ database migration pass.');
