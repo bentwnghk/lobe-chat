f5e3e330a9f7dfea812c3e797f47acebb981cdba
Ben <mr.ng@mystudies.net>
2024-10-29 21:18:24 +0800

    Rename

--- src/components/Error/loginRequiredNotification.tsx
+++ src/components/Error/loginRequiredNotification.tsx
@@ -1,4 +1,4 @@
-import { FluentEmoji } from '@lobehub/ui';
+import { FluentEmoji } from '@bentwnghk/ui';
 import { t } from 'i18next';
 
 import { notification } from '@/components/AntdStaticMethods';
