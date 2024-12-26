65782e7a5708146ae071569e21a5585232ed7d11
Ben <mr.ng@mystudies.net>
2024-12-26 15:30:32 +0800

    Update url.ts

--- src/const/url.ts
+++ src/const/url.ts
@@ -72,4 +72,4 @@ export const mailTo = (email: string) => `mailto:${email}`;
 export const AES_GCM_URL = 'https://datatracker.ietf.org/doc/html/draft-ietf-avt-srtp-aes-gcm-01';
 export const BASE_PROVIDER_DOC_URL = 'https://google.com/search?q=';
 export const SITEMAP_BASE_URL = isDev ? '/sitemap.xml/' : 'sitemap';
-export const CHANGELOG_URL = urlJoin(OFFICIAL_SITE, 'changelog/versions');
+export const CHANGELOG_URL = CHANGELOG;
