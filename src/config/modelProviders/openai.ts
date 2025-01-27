eada779d59dae3075ef4a91a3981857b19959b91
Ben Ng <mr.ng@mystudies.net>
2025-01-27 10:56:07 +0800

    Update openai.ts

--- src/config/modelProviders/openai.ts
+++ src/config/modelProviders/openai.ts
@@ -306,19 +306,6 @@ const OpenAI: ModelProviderCard = {
     {
       contextWindowTokens: 128_000,
       description:
-        'Pixtral 模型在图表和图理解、文档问答、多模态推理和指令遵循等任务上表现出强大的能力，能够以自然分辨率和宽高比摄入图像，还能够在长达 128K 令牌的长上下文窗口中处理任意数量的图像。',
-      displayName: 'Pixtral 12B',
-      enabled: true,
-      id: 'pixtral-12b',
-      pricing: {
-        input: 0.15,
-        output: 0.15,
-      },
-      vision: true,
-    },
-    {
-      contextWindowTokens: 128_000,
-      description:
         'Mistral Nemo是一个与Nvidia合作开发的12B模型，提供出色的推理和编码性能，易于集成和替换。',
       displayName: 'Mistral Nemo',
       enabled: true,
@@ -354,20 +341,6 @@ const OpenAI: ModelProviderCard = {
         input: 2,
         output: 6,
       },
-    },
-    {
-      contextWindowTokens: 128_000,
-      description:
-        'Pixtral Large 是一款拥有 1240 亿参数的开源多模态模型，基于 Mistral Large 2 构建。这是我们多模态家族中的第二款模型，展现了前沿水平的图像理解能力。',
-      displayName: 'Pixtral Large',
-      enabled: true,
-      functionCall: true,
-      id: 'pixtral-large-latest',
-      pricing: {
-        input: 2,
-        output: 6,
-      },
-      vision: true,
     },
     {
       contextWindowTokens: 128_000,
