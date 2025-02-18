b27d344b6d432537be29af6ded2f5d96b0d89d04
Zhijie He <hezhijie0327@hotmail.com>
2025-02-18 23:06:51 +0800

    💄 style: add `kimi-latest` for Moonshot (#6295)
    * 💄 style: add `kimi-latest` for Moonshot
    
    * 💄 style: update model list
    
    * 💄 style: update model desc and cachedInput
    
    * 💄 style: fix no model enabled
    
    * 💄 style: update check model

--- src/libs/agent-runtime/moonshot/index.ts
+++ src/libs/agent-runtime/moonshot/index.ts
@@ -28,13 +28,42 @@ export const LobeMoonshotAI = LobeOpenAICompatibleFactory({
     transformModel: (m) => {
       const model = m as unknown as MoonshotModelCard;
 
-      return {
-        enabled: LOBE_DEFAULT_MODEL_LIST.find((m) => model.id.endsWith(m.id))?.enabled || false,
-        functionCall: true,
-        id: model.id,
-        vision: model.id.toLowerCase().includes('vision'),
-      };
-    },
+    const functionCallKeywords = [
+      'moonshot-v1',
+      'kimi-latest',
+    ]
+
+    const visionKeywords = [
+      'kimi-latest',
+      'vision',
+    ]
+
+    const modelsPage = await client.models.list() as any;
+    const modelList: MoonshotModelCard[] = modelsPage.data;
+
+    return modelList
+      .map((model) => {
+        const knownModel = LOBE_DEFAULT_MODEL_LIST.find((m) => model.id.toLowerCase() === m.id.toLowerCase());
+
+        return {
+          contextWindowTokens: knownModel?.contextWindowTokens ?? undefined,
+          displayName: knownModel?.displayName ?? undefined,
+          enabled: knownModel?.enabled || false,
+          functionCall:
+            functionCallKeywords.some(keyword => model.id.toLowerCase().includes(keyword))
+            || knownModel?.abilities?.functionCall
+            || false,
+          id: model.id,
+          reasoning:
+            knownModel?.abilities?.reasoning
+            || false,
+          vision:
+            visionKeywords.some(keyword => model.id.toLowerCase().includes(keyword))
+            || knownModel?.abilities?.vision
+            || false,
+        };
+      })
+      .filter(Boolean) as ChatModelCard[];
   },
   provider: ModelProvider.Moonshot,
 });
