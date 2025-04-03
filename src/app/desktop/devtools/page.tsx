d334ea620dfa8e21b17984e0f477662b295a2cf6
Ben <mr.ng@mystudies.net>
2025-04-03 15:30:26 +0800

    Update page.tsx

--- src/app/desktop/devtools/page.tsx
+++ src/app/desktop/devtools/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { ActionIcon, FluentEmoji, SideNav } from '@lobehub/ui';
+import { ActionIcon, FluentEmoji, SideNav } from '@bentwnghk/ui';
 import { Cog, DatabaseIcon } from 'lucide-react';
 import { memo, useState } from 'react';
 import { Flexbox } from 'react-layout-kit';
