5d1c5e5df9b442a7aef56715d23bab9041471338
Ben <mr.ng@mystudies.net>
2025-04-13 15:22:20 +0800

    rename

--- src/app/[variants]/oauth/consent/[uid]/failed/page.tsx
+++ src/app/[variants]/oauth/consent/[uid]/failed/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Icon } from '@lobehub/ui';
+import { Icon } from '@bentwnghk/ui';
 import { Button, Card, Result } from 'antd';
 import { XCircle } from 'lucide-react';
 import Link from 'next/link';
